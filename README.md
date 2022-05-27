cffb3d4896469ced11c37c52318dbb8edc9c75a1
Leo Maza <leomaza2222@gmail.com>
2022-05-26 22:04:29 -0300

    Primera version casi sin css

---
+++ README.md
@@ -0,0 +1,4 @@
+# Reto_Quincenal_2
+
+Crear una pagina que permita marcar 20 casillas de verificacion con un orden 
+segun lo indique el usuario.
---
+++ algoritmo_retoQ.js
@@ -0,0 +1,21 @@
+const MIN_CASILLAS = 1;
+const MAX_CASILLAS = 20;
+
+document.querySelector("#ejecutar").setAttribute("onclick", "process()")
+let checkboxes = document.querySelector("#checkboxes");
+
+function process() {
+    checkboxes.innerHTML = "";
+    let alteracion = document.querySelector("#alteracion").value;
+
+    for (let i = 1; i <= 20; i++) {
+        checkboxes.innerHTML += `<label for="checkbox">checkbox Nro. ${i}</label>`
+        if (i % alteracion === 0) {
+            checkboxes.innerHTML += `<input type="checkbox" id="check" checked><br>`
+        } else {
+            checkboxes.innerHTML += `<input type="checkbox" id="check"><br>`
+        };
+    }
+}
+
+
